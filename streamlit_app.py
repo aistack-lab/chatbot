6a77d9baa251d93ee352d4776fef455a0f7fa34b
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-02-14 07:10:06 +0100

    chore: use llmling-agent

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ import asyncio
 import os
 
 import streamlit as st
+from llmling_agent import Agent
 from openai import OpenAI
 
 
@@ -20,32 +21,25 @@ async def run():
     else:
         client = OpenAI(api_key=openai_api_key)
         # Create a session state variable to store the chat messages.
-        if "messages" not in st.session_state:
-            st.session_state.messages = []
+        if "agent" not in st.session_state:
+            st.session_state.agent = Agent[None](model="gpt-3.5-turbo")
+        agent = st.session_state.agent
         # Display the existing chat messages via `st.chat_message`.
-        for message in st.session_state.messages:
-            with st.chat_message(message["role"]):
-                st.markdown(message["content"])
+        for message in agent._logger.message_history:
+            with st.chat_message(message.role):
+                st.markdown(str(message.content))
 
         # Create a chat input field to allow the user to enter a message.
         if prompt := st.chat_input("What is up?"):
-            # Store and display the current prompt.
-            st.session_state.messages.append({"role": "user", "content": prompt})
             with st.chat_message("user"):
                 st.markdown(prompt)
 
-            # Generate a response using the OpenAI API.
-            stream = client.chat.completions.create(
-                model="gpt-3.5-turbo",
-                messages=[
-                    {"role": m["role"], "content": m["content"]}
-                    for m in st.session_state.messages
-                ],
-                stream=True,
-            )
+            # async with agent.run_stream(prompt) as stream:
+            #     with st.chat_message("assistant"):
+            #         response = st.write_stream(stream.stream())
+            msg = await agent.run(prompt)
             with st.chat_message("assistant"):
-                response = st.write_stream(stream)
-            st.session_state.messages.append({"role": "assistant", "content": response})
+                st.markdown(msg.format())
 
 
 if __name__ == "__main__":
